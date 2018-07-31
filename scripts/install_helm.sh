699e6a4ecdaad1e90c1b28ff0f22ee1e0e915cfb
Brian Johnson <brian@divvycloud.com>
2018-07-31 16:02:44 -0400

    Adding install_helm convenience script

---
+++ scripts/install_helm.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+echo "Installing Helm"
+kubectl --namespace kube-system create sa tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
